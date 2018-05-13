7b1f6e61550c828c03196ea53e299425848f146b
John-Philip Johansson <seriema@gmail.com>
2018-05-13 16:39:43 +0200

    Select a real combo
    Give the user some time to get ready. Yellow and Orange belts only for now.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const practiceTime = 4000; // Milliseconds
-const belt = "yellow";
+
 const data = {
     tecniques: {
         "cross": "yellow",
@@ -7,11 +7,32 @@ const data = {
     },
     combos: {
         yellow: [
-            "cross, jab",
-            "cross, jab, cross, jab"
+            "jab, cross",
+            "jab, cross, jab, cross",
+            "jab, cross, hook",
+            "jab, cross, hook, cross",
+            "low hook, high hook, cross",
+            "jab, cross, roundhouse",
+            "front kick, roundhouse",
+            "jab, cross, knee",
+            "jab, cross, knee, elbow",
+            "jab, uppercut, knee"
         ],
         orange: [
-            "cross, jab, knee"
+            "jab, rear hand hook, lead hand uppercut",
+            "lead hand hook, cross, lead hand hook",
+            "jab, cross, change guard, cross",
+            "lead hand uppercut, lead hand hook, cross",
+            "rear hand uppercut, lead hand hook, cross",
+            "jab, lead hand backfist, cross",
+            "jab, cross, lead foot roundhouse",
+            "jab, cross, lead hand hook, back foot roundhouse",
+            "jab, cross, back foot roundhouse, lead knee",
+            "jab, front kick, cross, lead hand hook",
+            "jab, cross, sidekick",
+            "jab, cross, knee",
+            "jab, cross, axe kick",
+            "jab, cross, roundhouse"
         ]
     }
 };
@@ -24,23 +45,27 @@ let textNode = document.querySelector("#current-combo");
 var synth = window.speechSynthesis;
 
 const coachUser = combo => {
-    textNode.innerText = combo;
+    textNode.innerHTML = combo.split(", ").join("<br>");
     synth.speak(new SpeechSynthesisUtterance(combo));
 }
 
 
 // LOOP THE COMBO MOVES
-const nextCombo = (current) => {
+const nextCombo = (current, belt) => {
         if (current >= data.combos[belt].length) {
             coachUser("WELL DONE!");
         } else {
-            const combo = data.combos[belt][current];
-            coachUser(combo);
-            setTimeout(nextCombo.bind(null, current +1), practiceTime);
+            if (current < 0) {
+                coachUser("Get ready!");
+            } else {
+                const combo = data.combos[belt][current];
+                coachUser(combo);
+            }
+            setTimeout(nextCombo.bind(null, current + 1, belt), practiceTime);
         }
 }
 
-function startTraining() {
-    nextCombo(0);
+function startTraining(belt) {
+    nextCombo(-1, belt);
     document.querySelector(".wait-for-user").style.display = "none";
 }
