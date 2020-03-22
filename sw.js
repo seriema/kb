09733b75cdeba8677ae0b39e9780e5890b4d801b
John-Philip Johansson <seriema@gmail.com>
2020-03-22 10:01:35 +0100

    Release v1.1.1: PWA cache should update
    When there's a new version that should be fetched and cached.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'kb-v1.1.0';
+const cacheName = 'kb-v1.1.1';
 
 // Cache necessary files.
 self.addEventListener('install', function (event) {
@@ -15,11 +15,18 @@ self.addEventListener('install', function (event) {
     );
 });
 
-// Intercept fetch and check the cache before fetching.
+// Intercept fetch and check the cache before fetching, otherwise fetch and cache it.
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
+        caches.match(event.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + event.request.url);
+            return r || fetch(event.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + event.request.url);
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
